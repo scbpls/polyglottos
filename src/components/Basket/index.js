196961513aceec3a0451225297b370d42a002502
scbpls <44868129+scbpls@users.noreply.github.com>
2022-01-17 19:19:45 +0100

    Initial commit

--- src/components/Basket/index.js
+++ src/components/Basket/index.js
@@ -14,20 +14,30 @@ const Basket = () => {
       <main id="basket">
         <div>
           <h2>Basket</h2>
-          <section>
-            <div>
-              <h4>Starter:</h4>
-              <p>{basket.offer1}x $4,79</p>
-            </div>
-            <div>
-              <h4>Amateur:</h4>
-              <p>{basket.offer2}x $11,79</p>
-            </div>
-            <div>
-              <h4>Proffesional:</h4>
-              <p>{basket.offer3}x $40,79</p>
-            </div>
-          </section>
+          {basket.offer1 !== 0 && basket.offer2 !== 0 && basket.offer3 !== 0 ? (
+            <section>
+              {basket.offer1 !== 0 ? (
+                <div>
+                  <h4>Starter:</h4>
+                  <p>{basket.offer1}x $4,79</p>
+                </div>
+              ) : undefined}
+              {basket.offer1 !== 0 ? (
+                <div>
+                  <h4>Amateur:</h4>
+                  <p>{basket.offer2}x $11,79</p>
+                </div>
+              ) : undefined}
+              {basket.offer1 !== 0 ? (
+                <div>
+                  <h4>Proffesional:</h4>
+                  <p>{basket.offer3}x $40,79</p>
+                </div>
+              ) : undefined}
+            </section>
+          ) : (
+            <p>Empty</p>
+          )}
           <div>
             <h3>Together:</h3>
             <p>${sum()}</p>
