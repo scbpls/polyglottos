128da22eddeb719b4ac9e84f39f05d3b17fdc2e0
scbpls <44868129+scbpls@users.noreply.github.com>
2022-01-21 17:49:27 +0100

    Update quotes.js

--- src/utils/quotes.js
+++ src/utils/quotes.js
@@ -1,27 +1,23 @@
 export const quotes = [
   {
-    title: 'chuj pizda dupa cyce',
-    author: 'mgr inż. Dominik Strugała',
-  },
-  {
-    title: 'The limits of my language are the limits of my world XD',
+    title: 'The limits of my language are the limits of my world',
     author: 'Ludwig Wittgenstein',
   },
   {
-    title: 'The more language you know the more you are human XD',
+    title: 'The more language you know the more you are human',
     author: 'Tomáš Masaryk',
   },
   {
-    title: 'With languages, you are at home anywhere XD',
+    title: 'With languages, you are at home anywhere',
     author: 'Edward De Waal',
   },
   {
     title:
-      'I speak Spanish to God, Italian to women, French to men, and German to my horse XD',
+      'I speak Spanish to God, Italian to women, French to men, and German to my horse',
     author: 'Charles V',
   },
   {
-    title: 'A different language is a different vision of life XD',
+    title: 'A different language is a different vision of life',
     author: 'Federico Fellini',
   },
 ];
